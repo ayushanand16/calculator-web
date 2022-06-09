58753a1469c6433effba5c6cdbabfa3026f94689
ayushanand16 <96689639+ayushanand16@users.noreply.github.com>
2022-06-09 16:47:25 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -3,39 +3,34 @@ let answer = document.getElementById('result');
 
 let calculate = (number) => {
   let b = document.getElementById('eq');
-  if(b.value == "yes"){
+  if (b.value == "yes") {
     char = answer.value;
     char = char.slice(-1);
-    if((char >='0' && char <= '9')){
+    if ((char >= '0' && char <= '9')) {
       answer.value += number;
-    }
-    else{
-      if(isNaN(number)){
-        answer.value = answer.value.substring(0,answer.value.length-1);
-        if(answer.value===""){
-          if(number != '*' && number != '/' && number !='+')
+    } else {
+      if (isNaN(number)) {
+        answer.value = answer.value.substring(0, answer.value.length - 1);
+        if (answer.value === "") {
+          if (number != '*' && number != '/' && number != '+')
             answer.value += number;
-          }
-        else{
+        } else {
           answer.value += number;
         }
 
-      }
-      else{
-        answer.value +=number;
-      }
-    }
-  }
-  else{
-      if(isNaN(number)){
-        b.value="yes";
-        answer.value+=number;
-      }
-      else{
-        answer.value = "";
-        b.value = "yes";
+      } else {
         answer.value += number;
       }
+    }
+  } else {
+    if (isNaN(number)) {
+      b.value = "yes";
+      answer.value += number;
+    } else {
+      answer.value = "";
+      b.value = "yes";
+      answer.value += number;
+    }
 
   }
 }
@@ -43,30 +38,30 @@ let calculate = (number) => {
 let sqrt = () => {
   const b = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
-  entry.innerHTML = "sqrt("+answer.value+")";
-    b.appendChild(entry);
-      answer.value = Math.sqrt(answer.value);
+  entry.style.color = "white";
+  entry.innerHTML = "sqrt(" + answer.value + ")";
+  b.appendChild(entry);
+  answer.value = eval(answer.value);
+  answer.value = Math.sqrt(answer.value);
 }
 
 let sin = (ele) => {
   var v = ele.value
   const b = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
-  if(v=="inv")
-  {
+  entry.style.color = "white";
+  if (v == "inv") {
 
-  entry.innerHTML = "sin("+answer.value+")";
-  answer.value = Math.sin(answer.value);
-}
-  else
-  {
+    entry.innerHTML = "sin(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = Math.sin(answer.value);
+  } else {
 
-  entry.innerHTML = "asin("+answer.value+")";
-  answer.value = Math.asin(answer.value);
-}
-b.appendChild(entry);
+    entry.innerHTML = "asin(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = Math.asin(answer.value);
+  }
+  b.appendChild(entry);
 
 }
 
@@ -78,30 +73,29 @@ let rad = () => {
   var b = document.getElementById('rad_tog');
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
-  if(b.value == "rad")
-  {
+  entry.style.color = "white";
+  if (b.value == "rad") {
 
-  b.value= "deg";
-  b.innerHTML = "deg";
-  entry.innerHTML = "rad("+answer.value+")";
-    answer.value = (answer.value*Math.PI)/180;
-  }
-  else {
-    answer.value = (answer.value*180)/Math.PI;
+    b.value = "deg";
+    b.innerHTML = "deg";
+    entry.innerHTML = "rad(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = (answer.value * Math.PI) / 180;
+  } else {
+    answer.value = eval(answer.value);
+    answer.value = (answer.value * 180) / Math.PI;
     b.value = "rad";
     b.innerHTML = "rad";
-    entry.innerHTML = "deg("+answer.value+")";
+    entry.innerHTML = "deg(" + answer.value + ")";
   }
   k.appendChild(entry);
 
 }
 
 let brk = (b) => {
-  if(b.value == 'left'){
+  if (b.value == 'left') {
     answer.value += '(';
-  }
-  else{
+  } else {
     answer.value += ')';
   }
 }
@@ -109,51 +103,54 @@ let brk = (b) => {
 let tan = (ele) => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
+  entry.style.color = "white";
   var v = ele.value;
-  if(v=="inv"){
+  if (v == "inv") {
 
-  entry.innerHTML = "tan("+answer.value+")";
-  answer.value = Math.tan(answer.value);
+    entry.innerHTML = "tan(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = Math.tan(answer.value);
+  } else {
+    entry.innerHTML = "atan(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = Math.atan(answer.value);
   }
-  else{
-  entry.innerHTML = "atan("+answer.value+")";
-  answer.value = Math.atan(answer.value);
-  }
-    k.appendChild(entry);
+  k.appendChild(entry);
 }
 
 let cos = (ele) => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
+  entry.style.color = "white";
   var v = ele.value;
-  if(v=="inv"){
-  entry.innerHTML = "cos("+answer.value+")";
-  answer.value = Math.cos(answer.value);
+  if (v == "inv") {
+    entry.innerHTML = "cos(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = Math.cos(answer.value);
+  } else {
+    entry.innerHTML = "acos(" + answer.value + ")";
+    answer.value = eval(answer.value);
+    answer.value = Math.acos(answer.value);
   }
-  else{
-  entry.innerHTML = "acos("+answer.value+")";
-  answer.value = Math.acos(answer.value);
-  }
-    k.appendChild(entry);
+  k.appendChild(entry);
 }
 
 let ln = () => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
-    entry.innerHTML = "ln("+answer.value+")";
-    k.appendChild(entry);
+  entry.style.color = "white";
+  entry.innerHTML = "ln(" + answer.value + ")";
+  k.appendChild(entry);
   answer.value = Math.log(answer.value);
 }
 
 let log = () => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
-    entry.innerHTML = "log("+answer.value+")";
-    k.appendChild(entry);
+  entry.style.color = "white";
+  entry.innerHTML = "log(" + answer.value + ")";
+  k.appendChild(entry);
+  answer.value = eval(answer.value);
   answer.value = Math.log10(answer.value);
 
 }
@@ -161,81 +158,77 @@ let log = () => {
 let recipro = () => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-  entry.style.color="white";
-    entry.innerHTML = "1/("+answer.value+")";
-    k.appendChild(entry);
-  answer.value = 1/(answer.value);
+  entry.style.color = "white";
+  entry.innerHTML = "1/(" + answer.value + ")";
+  k.appendChild(entry);
+  answer.value = 1 / (answer.value);
 }
 
 let expo = () => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-    entry.innerHTML = "e^("+answer.value+")";
-    k.appendChild(entry);
+  entry.innerHTML = "e^(" + answer.value + ")";
+  k.appendChild(entry);
+  answer.value = eval(answer.value);
   answer.value = Math.exp(answer.value);
 }
 
 
 let nap = () => {
-  if(answer.value.length==0){
+  if (answer.value.length == 0) {
     answer.value += Math.E;
-  }
-  else{
-  char = String(answer.value);
-  char = char.slice(-1);
-  if(char >= '0' && char <= '9' ){
-    answer.value += '*'+Math.E;
-  }
-  else{
-    answer.value += Math.E;
-  }
+  } else {
+    char = String(answer.value);
+    char = char.slice(-1);
+    if (char >= '0' && char <= '9') {
+      answer.value += '*' + Math.E;
+    } else {
+      answer.value += Math.E;
+    }
   }
 }
 
 let pie = () => {
-  if(answer.value.length==0){
+  if (answer.value.length == 0) {
     answer.value += Math.PI;
-  }
-  else{
-  char = String(answer.value);
-  char = char.slice(-1);
-  if(char >= '0' && char <= '9' ){
-    answer.value += ('*'+Math.PI);
-  }
-  else{
-    answer.value += Math.PI;
-  }
+  } else {
+    char = String(answer.value);
+    char = char.slice(-1);
+    if (char >= '0' && char <= '9') {
+      answer.value += ('*' + Math.PI);
+    } else {
+      answer.value += Math.PI;
+    }
   }
 }
 
 let bs = () => {
-  answer.value = answer.value.slice(0,-1) ;
+  answer.value = answer.value.slice(0, -1);
 }
 let mod = () => {
-  if(answer.value.length!=0){
-    if(Math.sign(answer.value)==-1)
-    answer.value = answer.value*(-1);
+  if (answer.value.length != 0) {
+    if (Math.sign(answer.value) == -1)
+      answer.value = answer.value * (-1);
   }
 }
 
 let inv = () => {
   var k = document.getElementById('inverse');
   var ele_list = document.getElementsByClassName('inv');
-  if(k.value == "inv"){
+  if (k.value == "inv") {
     k.value = "a_inv";
-    for(var i=0 ; i< ele_list.length ; i++){
-      ele_list[i].value="a_inv";
+    for (var i = 0; i < ele_list.length; i++) {
+      ele_list[i].value = "a_inv";
       ele_list[i].innerHTML = ele_list[i].innerHTML + "-1".sup();
     }
     k.innerHTML = "NOR";
-  }
-  else{
+  } else {
     var ele_list = document.getElementsByClassName('inv');
-    k.value="inv";
-    for(var i=0 ; i< ele_list.length ; i++){
-      ele_list[i].value="inv";
+    k.value = "inv";
+    for (var i = 0; i < ele_list.length; i++) {
+      ele_list[i].value = "inv";
       char = String(ele_list[i].innerHTML);
-      ele_list[i].innerHTML= char.slice(0,3);
+      ele_list[i].innerHTML = char.slice(0, 3);
     }
     k.innerHTML = "INV";
   }
@@ -244,14 +237,15 @@ let inv = () => {
 let pow = () => {
   const k = document.getElementById('his');
   const entry = document.createElement('li');
-    entry.innerHTML = "("+answer.value+")^2";
-    k.appendChild(entry);
-  answer.value = Math.pow(answer.value,2);
+  entry.innerHTML = "(" + answer.value + ")^2";
+  k.appendChild(entry);
+  answer.value = eval(answer.value);
+  answer.value = Math.pow(answer.value, 2);
 }
 
 let tpow = () => {
-  if(answer.value.length>0)
-  answer.value += "**";
+  if (answer.value.length > 0)
+    answer.value += "**";
 }
 
 let output = (ele) => {
@@ -264,18 +258,17 @@ let output = (ele) => {
 }
 
 function openNav() {
-var x = window.matchMedia("(max-width: 700px)")
-nav(x);
-// x.addListener(nav)
+  var x = window.matchMedia("(max-width: 700px)")
+  nav(x);
+  // x.addListener(nav)
 
 }
 
-function nav(x){
-  if(x.matches){
+function nav(x) {
+  if (x.matches) {
     document.getElementById("hisbar").style.width = "100%";
     document.getElementById("result").style.marginTop = "30px";
-  }
-  else{
+  } else {
     document.getElementById("hisbar").style.width = "250px";
   }
 }
@@ -285,18 +278,17 @@ function closeNav() {
 }
 
 let sw = (ele) => {
-  let c=document.getElementById('gen');
-  let g=document.getElementById('calculator');
-  if(ele.value=="graph"){
+  let c = document.getElementById('gen');
+  let g = document.getElementById('calculator');
+  if (ele.value == "graph") {
     c.style.display = "none";
-    ele.innerHTML= "Calculator";
+    ele.innerHTML = "Calculator";
     ele.value = "calcu";
     g.className = "gr2";
 
-  }
-  else{
+  } else {
     g.className = "gr";
-    ele.innerHTML= "Graph Plotter";
+    ele.innerHTML = "Graph Plotter";
     ele.value = "graph";
     c.style.display = "flex";
   }
@@ -304,5 +296,5 @@ let sw = (ele) => {
 
 const chk = document.getElementById('yaha');
 chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark-mode');
+  document.body.classList.toggle('dark-mode');
 });
